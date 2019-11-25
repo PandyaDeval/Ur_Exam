634c56634bd5d8571637f7345c52805b40169e1d
PandyaDeval <dev23199@yahoo.in>
2019-11-25 15:47:20 +0530

    calculate marks

--- test_end.php
+++ test_end.php
@@ -37,6 +37,11 @@ while($que=mysqli_fetch_array($exam_qry)){
     $x++;
 }
 
-echo "Marks obtained: $user_marks<br>Total Marks: $marks";
+echo "Marks obtained: $user_marks<br>Total Marks: $marks<br><br> <button onclick='location=\"student_home.php\"'>Back to Home</button>";
+
+$qry = "select * from user where username='$username'";
+$user_id = mysqli_fetch_array(mysqli_query($con,$qry))[0];
+
+$insert_result=mysqli_query($con,"insert into `result`(`user_id`,`exam_id`,`user_marks`,`total_marks`) values('$user_id','$exam_id','$user_marks','$marks')");
 
 ?>
