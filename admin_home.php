4d709e70188c279ed8a6b94029b4aad196fddb4a
jrt1899 <jaytandel1899@gmail.com>
2019-11-25 10:26:52 +0530

    added admin_home.php

---
+++ admin_home.php
@@ -0,0 +1,27 @@
+<?php
+	$con=mysqli_connect("localhost","root","")or
+	die("Could not connect.");
+		mysqli_select_db($con,"urexam")or
+	die("Could not select database.");
+
+	$username = "jay";
+
+	$admin_profile_query = "select * from admin where username = '$username'";
+	$admin_profile_run=mysqli_query($con,$admin_profile_query);
+	$admin_profile_row = mysqli_fetch_array($admin_profile_run);
+
+	$name = $admin_profile_row['name'];
+	$email = $admin_profile_row['email'];
+
+	echo "<center>";
+
+	echo "WELCOME ADMIN - $username     <br><br>";
+	echo "$name<br><br>$email<br><br>     ";
+	echo "<a href = ' add_que.php '>ADD QUESTION</a>     ";
+	echo "<a href = ' add_test.php '>ADD TEST</a>     ";
+	echo "<a href = ' see_user.php '>MANIPULATE USER</a>     ";
+	
+
+	echo "</center>"
+
+?>
