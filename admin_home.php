84a39e9006e2ac00aa8b013c5309a408fd178a43
jrt1899 <jaytandel1899@gmail.com>
2019-11-25 13:09:29 +0530

    admin_add_que

--- admin_home.php
+++ admin_home.php
@@ -1,11 +1,11 @@
 <?php
+	session_start();
 	$con=mysqli_connect("localhost","root","")or
 	die("Could not connect.");
 		mysqli_select_db($con,"urexam")or
 	die("Could not select database.");
 
 	$username = $_SESSION['username'];
-	//comment
 
 	$admin_profile_query = "select * from admin where username = '$username'";
 	$admin_profile_run=mysqli_query($con,$admin_profile_query);
@@ -18,7 +18,7 @@
 
 	echo "WELCOME ADMIN - $username     <br><br>";
 	echo "$name<br><br>$email<br><br>     ";
-	echo "<a href = ' add_que.php '>ADD QUESTION</a>     ";
+	echo "<a href = ' add_que.html '>ADD QUESTION</a>     ";
 	echo "<a href = ' add_test.php '>ADD TEST</a>     ";
 	echo "<a href = ' see_user.php '>MANIPULATE USER</a>     ";
 	
