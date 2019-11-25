e7a84bf4e626a546b0a9a388510a723adf2c2b2c
PandyaDeval <dev23199@yahoo.in>
2019-11-25 12:59:00 +0530

    added subject

--- student_home.php
+++ student_home.php
@@ -11,23 +11,43 @@ $con=mysqli_connect("localhost","root","")or
     $qry = "SELECT * FROM user where username = '$username'";
     $row = mysqli_fetch_array(mysqli_query($con,$qry));
 
-    $sub_qry = "select distinct(subject) from exam";
+    $sub_qry = "select distinct(subject) from question_bank";
     $sub_qry_result = mysqli_query($con, $sub_qry);
+
+    $exam_qry = "select * from exam";
+    $exam_qry_result = mysqli_query($con,$exam_qry);
 
     
 ?>
 
 <html>
+    <head>
+        <script>
+            function sub_select(){
+                var subject = document.getElementById("sub_select").value;
+                if(subject != "blank"){
+                    document.getElementById("select_div").html += ""
+                }
+            }
+        </script>
+    </head>
     <body>
         <center><h1>Welcome <?php echo $row[3];?></h1>
 
         <br><br>
 
-        <?php
-        while($row=mysqli_fetch_array($sub_qry_result)){
-            echo "$row[0]\n";
-        }
-        ?>
+        <div id="select_div">
+            Select Subject:
+            <select onchange = "sub_select()" id="sub_select">
+                <option value='blank'>Select Subject</option>
+            <?php
+            while($row=mysqli_fetch_array($sub_qry_result)){
+                echo "<option value='$row[0]'>$row[0]</option>";
+            }
+            ?>
+            </select>
+        </div>
+
 
         </center>
     </body>
