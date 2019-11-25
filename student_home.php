9fb60637a6d03054701421200c3dfc50a5e45fa3
PandyaDeval <dev23199@yahoo.in>
2019-11-25 13:41:22 +0530

    select exam

--- student_home.php
+++ student_home.php
@@ -25,9 +25,30 @@ $con=mysqli_connect("localhost","root","")or
         <script>
             function sub_select(){
                 var subject = document.getElementById("sub_select").value;
-                if(subject != "blank"){
-                    document.getElementById("select_div").html += ""
+                if(subject == "blank"){
+                    document.getElementById("show_exams").html = "";
                 }
+                else{
+                    if (window.XMLHttpRequest) {
+                    // code for IE7+, Firefox, Chrome, Opera, Safari
+                    xmlhttp = new XMLHttpRequest();
+                    } else {
+                        // code for IE6, IE5
+                        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                    }
+                    xmlhttp.onreadystatechange = function() {
+                        if (this.readyState == 4 && this.status == 200) {
+                            document.getElementById("show_exams").innerHTML = this.responseText;
+                        }
+                    };
+                    xmlhttp.open("GET","get_exams.php?s="+subject,true);
+                    xmlhttp.send();
+                }
+            }
+
+            function start_exam(){
+                var exam = document.getElementById("select_exam").value;
+                location = "exam_page.php?exam="+exam;
             }
         </script>
     </head>
@@ -36,7 +57,7 @@ $con=mysqli_connect("localhost","root","")or
 
         <br><br>
 
-        <div id="select_div">
+        <div>
             Select Subject:
             <select onchange = "sub_select()" id="sub_select">
                 <option value='blank'>Select Subject</option>
@@ -46,6 +67,10 @@ $con=mysqli_connect("localhost","root","")or
             }
             ?>
             </select>
+        </div>
+        <br><br><br>
+        <div id='show_exams'>
+            
         </div>
 
 
